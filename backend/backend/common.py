72300efbeb1ea034427bef0f1fb9ceecbc707c48
alvinsss <6449694@qq.com>
2022-12-20 10:39:38 +0800

    注册和登录

---
+++ backend/backend/common.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/12/20 9:46
+# @Author  : alvin
+# @File    : common.py
+# @Software: PyCharm
+class Error:
+    """
+    子定义错误码与错误信息
+    """
+    TOKEN_ERROR = {"10300", "认证失败"}
+    USER_OR_PAWD_NULL = {"10010": "用户名密码为空"}
+    USER_OR_PAWD_ERROR = {"10011": "用户名密码错误"}
+    PAWD_ERROR = {"10012": "两次密码不一致"}
+    USER_EXIST = {"10013": "用户已被注册"}
+
+def response(success:bool=True,error=None ,result=[]) -> dict:
+    '''
+    定义统一返回格式
+    '''
+    print(success,error,result)
+    if error is None:
+        error_code = ""
+        error_msg = ""
+    else:
+        success = False
+        error_code = list(error.keys())[0]
+        error_msg = list(error.values())[0]
+
+    resp_data = {
+        "success": success,
+        "error": {
+            "code": error_code,
+            "msg": error_msg
+        },
+        "result":result
+    }
+    return resp_data
--- backend/users/api.py
+++ backend/users/api.py
@@ -1,5 +1,48 @@
 from ninja import  Router
-# Create your tests here.
+from ninja import  Schema
+from django.contrib.auth.models import  User
+from django.contrib import auth
 
+from backend.common import response,Error
 
 router = Router(tags=["users"])
+
+class RegisterIN(Schema):
+    username:str
+    password:str
+    confirm_pwd:str
+
+@router.post("/register")
+def user_register(request,payload:RegisterIN):
+    if payload.password != payload.confirm_pwd:
+        return response(Error.PAWD_ERROR)
+    user = User.objects.filter(username=payload.username)
+    print(user,type(user))
+    if user:
+        return response(error=Error.USER_EXIST)
+    user = User.objects.create_user(username=payload.username,password=payload.password)
+    user_info={
+        "id":user.id,
+        "username":user.username,
+    }
+    return response(result=user_info)
+
+
+class LoginIN(Schema):
+    username:str
+    password:str
+
+@router.post("/login")
+def user_login(request,payload:LoginIN):
+    user_name=payload.username
+    user_pwd=payload.password
+    print(user_name,user_pwd)
+    user =auth.authenticate(username=user_name,password=user_pwd)
+    if (user is not None) and (user.is_active is True):
+        user_info={
+            "id":user.id,
+            "username":user.username,
+        }
+        return response(result=user_info)
+    else:
+        return response(error=Error.USER_OR_PAWD_ERROR)
