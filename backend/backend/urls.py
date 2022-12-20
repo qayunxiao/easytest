4fec70d1d1ff105b5792f17eff0c80d2e7461b94
alvinsss <6449694@qq.com>
2022-12-20 09:28:10 +0800

    使用父类的urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from  backend import  api
+from  backend.api import  api
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', api.api),
+    path('api/', api.urls),
 ]
